308a03ef3b35c6fbde68986f0f5f88d80a53e31a
Dan Xu <danxu@Dans-MacBook-Pro.local>
2019-11-05 12:35:35 -0800

    All the tests passed

--- spec/lib/searchYouTubeSpec.js
+++ spec/lib/searchYouTubeSpec.js
@@ -3,6 +3,7 @@ import YOUTUBE_API_KEY from '../../src/config/youtube.js';
 import exampleVideoData from '../../src/data/exampleVideoData.js';
 
 var getURLSearchParams = function(url) {
+  console.log('url:', url);
   return url
     .split('?')[1]
     .split('&')
@@ -81,6 +82,7 @@ describe('searchYouTube', function() {
     xhr.restore();
 
     searchYouTube(options, (data) => {
+      console.log(data, exampleVideoData);
       expect(hasSameShape(data, exampleVideoData)).to.be.true;
       done();
     });
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = (options, callback = ()=>{}) => {
+  const ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
+
+  $.ajax({
+    url: ENDPOINT,
+    type: 'GET',
+    data: {
+      key: options.key,
+      maxResults: options.max,
+      videoEmbeddable: true,
+      q: options.query,
+      part: 'snippet',
+      type: 'video',
+    },
+    success: (data)=> {
+      callback(data.items);
+    },
+    error: function(error) {
+      console.error('Youtube: Failed to find a video', error);
+    }
+  });
 };
 
 export default searchYouTube;
